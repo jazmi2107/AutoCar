abd537d1ef434c1984e2ff1466683744dfa10ccf
AutoCar Developer <dev@autocar.com>
2026-01-28 03:09:30 +0800

    Configure TrustProxies and force HTTPS in production to fix Mixed Content errors

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\URL;
 use App\View\Composers\UserProfileComposer;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         // Share user with driver relationship across all user views
         View::composer('users.*', UserProfileComposer::class);
     }
